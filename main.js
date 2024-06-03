bfa7342e903960f4f1c54a8269dad47fc06ea478
luthfi30 <lurrah30@gmail.com>
2024-06-03 18:14:03 +0700

    vercel cors

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const session = require("express-session");
-const cors = require("cors");
-const corsConfig = {
-  origin: "*",
-  credential: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
-app.options("*", cors(corsConfig));
-app.use(cors(corsConfig));
+// const cors = require("cors");
+// const corsConfig = {
+//   origin: "*",
+//   credential: true,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+// };
+// app.options("*", cors(corsConfig));
+// app.use(cors(corsConfig));
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 4000;
 
